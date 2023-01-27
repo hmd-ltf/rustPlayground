9b81a0809b584c3e370ac2aed9c905e33c301f88
Hammad Latif <mhmd.ltf786@gmail.com>
2023-01-27 23:51:06 +0000

    Hello world project

---
+++ hello_world/target/debug/deps/hello_world-79129ff3c41ad4c8.d
@@ -0,0 +1,5 @@
+/workspace/rustPlayground/hello_world/target/debug/deps/hello_world-79129ff3c41ad4c8: src/main.rs
+
+/workspace/rustPlayground/hello_world/target/debug/deps/hello_world-79129ff3c41ad4c8.d: src/main.rs
+
+src/main.rs:
